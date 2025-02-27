44b1dd18e7a370c0dc6563cd7dbf3fdeb7641c28
elbert78 <elbert.gm7@gmail.com>
2025-02-27 09:42:41 +0700

    Week 2.5: Study Casee (Unfinished)

---
+++ noSEon.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int command;
+
+  cout << "1. Manage Todo List\n" << "2. View All Todo List\n" << "0. Exit\n";
+  cout << ">>";
+  cin >> command;
+
+  cout << command;
+
+  return 0;
+}
